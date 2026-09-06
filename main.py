5acfaafa3c451774341f6a9889fcfb7094465028
kapilkec <kapilvirat2030@gmail.com>
2026-09-06 22:16:20 +0530

    pipeline added

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+from src.pipelines.pipeline import run_research_pipeline
 from src.tools.tools import web_search, scrape_url
 
 def main():
     print("Hello from multi-agent!")
-    # result = web_search.invoke("What is life")
-    result = scrape_url.invoke("https://en.wikipedia.org/wiki/Artificial_general_intelligence")
-    print(result[:1000])  
+    run_research_pipeline("The impact of AI on healthcare")
 
 if __name__ == "__main__":
     main()
