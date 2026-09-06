9ed7bd7609bc20bcd38a32876ccd5b8ecc6f0ee7
kapilkec <kapilvirat2030@gmail.com>
2026-09-06 20:46:35 +0530

    Remove env file from tracking

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from src.tools.tools import web_search, scrape_url
+
 def main():
     print("Hello from multi-agent!")
-
+    # result = web_search.invoke("What is life")
+    result = scrape_url.invoke("https://en.wikipedia.org/wiki/Artificial_general_intelligence")
+    print(result[:1000])  
 
 if __name__ == "__main__":
     main()
